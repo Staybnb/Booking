0184bd5d603760db68393b6ec1446d123d749b5f
Ben Mangold <benmangold@gmail.com>
2018-11-07 11:08:20 -0500

    stubbing out crud endpoints.  must make booking table too

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const database = require('../database/index.js')
+const express = require('express')
 const path = require('path')
 const port = 4000;
 const morgan = require('morgan');
-const cors = require('cors')
 
 var app = express();
 
@@ -21,6 +21,22 @@ app.get('/apartment/:id', (req, res)=>{
 	}).catch((err)=>{
 		res.send(err)
 	})
+})
+
+app.post('/apartment/:id', (req, res) => {
+	console.log('post apartment listing')
+})
+
+app.delete('/apartment/:id', (req, res) => {
+	console.log('delete apartment listing')
+})
+
+app.get('/booking/:aptId', (req, res) => {
+	console.log('get bookings by apt id')
+})
+
+app.post('/booking/aptId', (req, res) => {
+	console.log('create a new apartment booking')
 })
 
 // app.use('/bundle.js', express.static(path.join(__dirname + '/../client/dist')));
