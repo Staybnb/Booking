cc77be287f9a7f25fb3fdf0273bcafc4fcf28578
Ben Mangold <benmangold@gmail.com>
2018-11-08 11:58:39 -0500

    testing bluebird promise

--- __test__/model.test.js
+++ __test__/model.test.js
@@ -1,5 +1,14 @@
-const dbConnection = require('../database/dbConnection.js')
 const db = require('../database/index.js')
+var Promise = require("bluebird");
+
+// bluebird seems to take care of opening and closing connection
+
+describe('Test db connection', () => {
+  test('db method should return a promise', () => {
+      expect(typeof db.getListings()).toBe('object')
+      return expect(db.getListings()).toBeInstanceOf(Promise)
+  });
+})
 
 function sum(a, b) {
   return a + b;
