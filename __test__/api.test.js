afcda53330ef07ec2f3ed0031657e799c917a98c
Ben Mangold <benmangold@gmail.com>
2018-11-08 11:13:39 -0500

    basic server testing

--- __test__/api.test.js
+++ __test__/api.test.js
@@ -1,5 +1,3 @@
-
-
 const request = require('supertest');
 const app = require('../server/app')
 describe('Test the root path', () => {
@@ -10,3 +8,22 @@ describe('Test the root path', () => {
         });
     });
 });
+
+describe('Test the sample client path', () => {
+  test('It should response the GET method', (done) => {
+      request(app).get('/listing?id=1').then((response) => {
+          expect(response.statusCode).toBe(200);
+          done();
+      });
+  });
+});
+
+describe('Test getting all listings', () => {
+  test('It should response the GET method', (done) => {
+      request(app).get('/api/listings').then((response) => {
+          expect(response.statusCode).toBe(200);
+          done();
+      });
+  });
+});
+
