55d95b0ecb0f4afe434ab946119a5783e16ebf9c
Ben Mangold <benmangold@gmail.com>
2018-11-09 16:42:31 -0500

    cleaning up echo

--- seed_mysql/seed.sh
+++ seed_mysql/seed.sh
@@ -1,5 +1,4 @@
 MYDIR="$(dirname "$(which "$0")")"
-ECHO "hi"
 mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
 mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
 mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
