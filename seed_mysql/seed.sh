79d0244018c5ad55dfefa738a15a9b677ff49593
Ben Mangold <benmangold@gmail.com>
2018-11-09 16:41:17 -0500

    seeding 50 million in mongo and mysql

--- seed_mysql/seed.sh
+++ seed_mysql/seed.sh
@@ -1,3 +1,16 @@
 MYDIR="$(dirname "$(which "$0")")"
 ECHO "hi"
 mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
+mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
+mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
+mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
+mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
+mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
+mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
+mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
+mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
+mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
+mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
+mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
+mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
+mysql -u root --local-infile < /Users/benjaminmangold/HackReactor/sb-Booking/seed_mysql/seed.sql;
