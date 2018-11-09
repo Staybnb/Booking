d0aab1e18a835ee2422dbb0be0024bceeae420bc
Ben Mangold <benmangold@gmail.com>
2018-11-09 15:35:20 -0500

    seeding mongo from csv

--- seed_mongo/seed.sh
+++ seed_mongo/seed.sh
@@ -1,1 +1,1 @@
-mongoimport -d mydb -c things --type csv --file db.csv --headerline
+mongoimport -d booking -c apartment --type csv --file /Users/benjaminmangold/HackReactor/sb-Booking/seed_mongo/sample.csv --headerline
