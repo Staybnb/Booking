15e6edf2cab2ef6178bc639892a175456b4df956
Ben Mangold <benmangold@gmail.com>
2018-11-09 18:59:09 -0500

    ..

--- generateCSV.js
+++ generateCSV.js
@@ -1,3 +1,12 @@
 const faker = require('faker');
 
 // this file must generate a .csv file to be used by each db
+
+// apartment.csv
+// price, minStay, stars, numRatings, max 
+// 10, 1, 123, 10, 1, 90210  
+// ...
+
+// dates.csv
+
+
