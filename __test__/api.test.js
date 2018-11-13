9ba053cb0019649512951317967bbcf7043b29a1
Ben Mangold <benmangold@gmail.com>
2018-11-12 20:04:22 -0500

    unit testing crud

--- __test__/api.test.js
+++ __test__/api.test.js
@@ -33,10 +33,10 @@ describe("Test getting all dates", () => {
   });
 });
 
-let insertId = 0;
+let listingId = 0;
 
 // test create and delete listing
-describe("Test posting and deleting a listing", function() {
+describe("Test posting a listing", function() {
   it("responds with json", function(done) {
     request(app)
       .post("/api/listing")
@@ -51,29 +51,31 @@ describe("Test posting and deleting a listing", function() {
       .expect(200)
       .end(function(err, res) {
         if (err) return done(err);
-        insertId = JSON.parse(res.text).insertId;
-        console.log("created listing " + insertId);
+        listingId = JSON.parse(res.text).listingId;
+        console.log("created listing " + listingId);
         done();
       });
   });
 });
 
 // test delete that listing
-describe("Test posting and deleting a listing", function() {
+describe("Test deleting a listing", function() {
   it("should delete the inserted listing", function(done) {
     request(app)
-      .delete("/api/listing/" + insertId)
+      .delete("/api/listing/" + listingId)
       .expect(200)
       .end(function(err, res) {
         if (err) return done(err);
-        console.log("deleted listing " + insertId);
+        console.log("deleted listing " + listingId);
         done();
       });
   });
 });
 
+let dateId = 0;
+
 // test create date
-describe("Test posting a listing", function() {
+describe("Test posting a date", function() {
   it("responds with json", function(done) {
     request(app)
       .post("/api/date")
@@ -82,9 +84,23 @@ describe("Test posting a listing", function() {
       .expect(200)
       .end(function(err, res) {
         if (err) return done(err);
+        dateId = JSON.parse(res.text).dateId;
+        console.log("created listing " + dateId);
         done();
       });
   });
 });
 
 // test delete that date
+describe("Test deleting a date", function() {
+  it("should delete the inserted listing", function(done) {
+    request(app)
+      .delete("/api/listing/" + dateId)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        console.log("deleted listing " + dateId);
+        done();
+      });
+  });
+});
